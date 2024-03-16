06e828a743405a77ea4aa16c740ec3e51f8e31f1
Prathamesh Chakote <30796045+Prathameshchakote@users.noreply.github.com>
2024-03-16 10:35:36 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Prathamesh on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
